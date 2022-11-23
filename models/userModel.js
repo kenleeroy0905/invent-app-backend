e18b74ffb85fdc770b03ca0ad689592d3626dc3e
Kenneth Leeroy <misakenleeroy@gmail.com>
2022-11-23 14:13:02 +0400

    first commit

---
+++ models/userModel.js
@@ -0,0 +1,60 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please add your name"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please add your email"],
+      unique: true,
+      trim: true,
+      match: [
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        "Please enter a valid email",
+      ],
+    },
+    password: {
+      type: String,
+      required: [true, "Please add your password"],
+      minLength: [6, "Password must be at least 6 characters"],
+      //maxLength: [30, "Password must not exceed 30 characters"],
+    },
+    photo: {
+      type: String,
+      required: [true, "Please add a photo"],
+      default: "https://i.ibb.co/4pDNDk1/avatar.png",
+    },
+    phone: {
+      type: String,
+      default: "+971",
+    },
+    bio: {
+      type: String,
+      maxLength: [250, "Bio must not be more than 250 characters"],
+      default: "bio",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+//encrypt password
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  //Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(this.password, salt);
+  this.password = hashedPassword;
+  next();
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const path = require("path");
+
+const userRoute = require("./routes/userRoute");
+const productRoute = require("./routes/productRoute");
+const contactRoute = require("./routes/contactRoute");
+
+const errorHandler = require("./middleware/errorMiddleware");
+
+const app = express();
+
+//Middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://invent-app.vercel.app"],
+    credentials: true,
+  })
+);
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+//Routes Middleware
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/contactus", contactRoute);
+
+//Routes
+app.get("/", (req, res) => {
+  res.send("Home Page");
+});
+
+// Error Handler Middleware
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+//Connect to MongoDb
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
