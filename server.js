1e2591dd474f6a1d0b703788fe37e8688564cf7c
Kenneth Leeroy <misakenleeroy@gmail.com>
2022-11-23 14:44:29 +0400

    second commit

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://invent-app.vercel.app"],
+    origin: ["http://localhost:3000", "https://invent-app-two.vercel.app/"],
     credentials: true,
   })
 );
